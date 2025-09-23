671d698f0f43e5eae1e689a58562a9aa33f24943
shahriartanvirnoor <96464274+shahriartanvirnoor@users.noreply.github.com>
2025-09-23 17:38:17 +0600

    Create reverse_array_input_is_given.c

---
+++ reverse_array_input_is_given.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+    {
+
+
+        int array[]={6,8,9,4,3,7,5,8,4,0,8,5,9,8,0,3,5,6,3,8,0,8,5,4,2,8,6,0,7};
+        printf("Reverse array : ");
+        for(int i=28; i>=0; i--)
+        {
+
+        printf(" %d ",array[i]);
+        }
+        return 0;
+
+    }
