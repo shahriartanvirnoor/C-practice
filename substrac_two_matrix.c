1c8e0b6f548f55e2a5856d43e01004548535855f
shahriartanvirnoor <96464274+shahriartanvirnoor@users.noreply.github.com>
2025-09-24 19:03:58 +0600

    Create substrac_two_matrix.c

---
+++ substrac_two_matrix.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int i=0, j=0;
+    int add[100][100];
+       int arr_1[3][3]={27, 83, 14, 56, 91, 42, 7, 68, 35};
+       int arr_2[3][3]={12, 79, 44, 6, 58, 33, 90, 21, 65};
+       printf("Substract of two matrix:\n");
+       for(i=0;i<3;i++)
+       {
+           for(j=0;j<3;j++)
+           {
+               add[i][j]=arr_1[i][j]-arr_2[i][j];
+               printf(" %d ",add[i][j]);
+           }
+            printf("\n");
+       }
+
+    return 0;
+}
