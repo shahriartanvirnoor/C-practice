8999b4994434a21e4d3770dd1fd697488c5df6d6
shahriartanvirnoor <96464274+shahriartanvirnoor@users.noreply.github.com>
2025-10-06 20:51:23 +0600

    Create squre_root.c
    this program is for to find out the squre root of a number in c

---
+++ squre_root.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+   double q, b;
+printf("Input Square: ");
+scanf("%lf", &b);
+   q= sqrt(b);
+   printf("\nsquare root =%lf",q);
+   return 0;
+
+}
