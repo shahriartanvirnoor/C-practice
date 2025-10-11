31e27b09f0ce073b3df1590e6ad617450bffd861
shahriartanvirnoor <96464274+shahriartanvirnoor@users.noreply.github.com>
2025-10-11 15:00:22 +0600

    Create bit_wise_operator.c

---
+++ bit_wise_operator.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#define A 25
+#define B 60
+int main()
+  {
+      printf("\n A % B =%d ",A,B,A&B);
+       printf("\n A | B = %d ",A|B);
+        printf("\n ~A = %d ",~A);
+         printf("\n ~B = %d ",~B);
+          printf("\n A>> 1= %d ",A>>1);
+           printf("\n B>>1 = %d ",B>>1);
+            printf("\n A>>2 = %d ",A>>2);
+             printf("\n B>>2 = %d ",B>>2);
+              printf("\n A<<1 = %d ",A<<1);
+               printf("\n B<<1 = %d ",B<<1);
+                printf("\n A<<2 = %d ",A<<2);
+                 printf("\n B<<2 = %d ",B<<2);
+
+    return 0;
+  }
