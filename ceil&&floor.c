61271c22d38ba52c21fc21cec545c93e24b52dbd
shahriartanvirnoor <96464274+shahriartanvirnoor@users.noreply.github.com>
2026-02-15 09:19:33 +0600

    Add program to demonstrate ceil and floor functions
    Implement a program to demonstrate ceil and floor functions.

---
+++ ceil&&floor.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+
+    int x, y;
+    float p, q;
+    p= 1.9;
+    q = 5.8;
+    printf("The ceil of %.2f is %f\n", ceil(p));// the work of ceil function is to go to the upper nearest float number
+    printf("The floor of %.2f is %f\n", floor(q));// the work of floor function is to go to the lower nearest float number
+    return 0;
+}
